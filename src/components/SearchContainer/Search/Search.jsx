5831b035e008aa46b7643dbe783470bfde0b8504
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-05-19 01:02:32 +0200

    Vista details echa y el debounce

--- src/components/SearchContainer/Search/Search.jsx
+++ src/components/SearchContainer/Search/Search.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Search.css";
 
 
 const Search = ({onSearch}) => {
-  //const [pokemonData, setPokemonData] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [debaunceValue, setDebounceValue] = useState("");
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
 
-const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(inputValue);
-      onSearch(inputValue); // invocar la función unSearch pasada por props
-      setInputValue("");
-    };
-  return (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={inputValue} placeholder="Busca un Pokemon" onChange={handleChange}/>
-      <button type="submit">Search</button>
-    </form>
-  </div>
-  )
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceValue(inputValue);
+    }, 2000); // tiempo de espera (2000ms)
+
+    return () => clearTimeout(timeout); // limpia si vuelve a escribir antes del tiempo
+  }, [inputValue]);
+
+  // Ejecutar la búsqueda cuando el valor debounced cambia
+  useEffect(() => {
+    if (debaunceValue.trim() !== "") {
+      onSearch(debaunceValue);
+    }
+  }, [debaunceValue, onSearch]);
+
+
+  // const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log(inputValue);
+  //     onSearch(inputValue); // invocar la función unSearch pasada por props
+  //     setInputValue("");
+  //   };
+return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        placeholder="Busca un Pokémon"
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
+
 
 export default Search;
